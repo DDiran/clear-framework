1371af859fd5070157f739321814ed99fe1f3d19
David Lawson <dlawson@veracode.com>
2023-05-03 12:06:14 +0100

    Add the gpt2 model as an alternative

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-from prompts import INITIAL_PROMPT, RESPONSE
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from prompts import INITIAL_PROMPT, GPT_2_PROMPT, RESPONSE
 import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+model_name = "gpt2"
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+
 app = Flask(__name__)
 
-def analyze_prompt(prompt):
+def analyze_prompt_with_chatgpt(prompt):
     response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
@@ -19,11 +24,31 @@ def analyze_prompt(prompt):
 )
     return response.choices[0]['message']['content']
 
+def analyze_prompt_with_gpt2(prompt, max_length=1000, num_return_sequences=1):
+    combined_prompt = f"{GPT_2_PROMPT} {prompt}\n\nEVALUATION:"
+    inputs = tokenizer.encode(combined_prompt, return_tensors="pt")
+    outputs = model.generate(
+        inputs,
+        max_length=max_length,
+        num_return_sequences=num_return_sequences,
+        no_repeat_ngram_size=2,
+        temperature=0.7,
+        top_k=50,
+        top_p=0.95,
+        do_sample=True,
+    )
+    generated_text = [
+        tokenizer.decode(output, skip_special_tokens=True) for output in outputs
+    ]
+    return generated_text
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         prompt = request.form.get('prompt')
-        result = analyze_prompt(prompt)
+        result = analyze_prompt_with_chatgpt(prompt)
         return jsonify(result)
     return render_template('index.html')
 
